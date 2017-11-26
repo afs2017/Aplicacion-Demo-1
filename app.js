c1966e8dc17dafcb327ce2f334e270c237d94f6c
afs2017 <32643646+afs2017@users.noreply.github.com>
2017-11-26 19:26:58 +0100

    2
    2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require ('express-validator');
+var expressSession = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -20,6 +22,8 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(expressSession({secret:'max', saveUninitialized: false, resave: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
